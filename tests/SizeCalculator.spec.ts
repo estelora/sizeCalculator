3fb112438b311a482f3f2053e2cacc99bff773db
estelora <lydia@lejoslin.com>
2024-07-24 15:48:33 -0400

    Added tests that verify sizes for a set of measurements in range.

--- tests/SizeCalculator.spec.ts
+++ tests/SizeCalculator.spec.ts
@@ -65,7 +65,7 @@ test.describe('SizeCalculator', () => {
     await expect(errorMessage).toBeVisible();
   });
 
-  test('should show that when measurements are entered and the "See My Sizes" button is clicked that "Closest Size Matches" is displayed', async ({ page }) => {
+  test('should show when measurements are entered and the "See My Sizes" button is clicked that "Closest Size Matches" is displayed', async ({ page }) => {
     await page.fill('#bust', '36.5');
     await page.fill('#waist', '30');
     await page.fill('#hips', '39.5');
@@ -74,4 +74,49 @@ test.describe('SizeCalculator', () => {
     const sizeMatches = page.getByText('Closest Size Matches');
     await expect(sizeMatches).toBeVisible();
   });
+
+  test('should show when in-range measurements (bust 36.5, waist 30, hips 39.5) are entered and the "See My Sizes" button is clicked that Size 8 for the Zara brand is displayed', async ({ page }) => {
+      await page.fill('#bust', '36.5');
+      await page.fill('#waist', '30');
+      await page.fill('#hips', '39.5');
+      await page.getByRole('button').click();
+
+      const zaraSize8 = page.getByText('Size 8').nth(0); 
+      await zaraSize8.scrollIntoViewIfNeeded();
+      await expect(zaraSize8).toBeVisible();
+    });
+  
+  test('should show when in-range measurements (bust 36.5, waist 30, hips 39.5) are entered and the "See My Sizes" button is clicked that Size 8 for the Talbots brand is displayed', async ({ page }) => {
+      await page.fill('#bust', '36.5');
+      await page.fill('#waist', '30');
+      await page.fill('#hips', '39.5');
+      await page.getByRole('button').click();
+
+      const talbotsSize8 = page.getByText('Size 8').nth(1); 
+      await talbotsSize8.scrollIntoViewIfNeeded();
+      await expect(talbotsSize8).toBeVisible();
+    });
+
+  test('should show when in-range measurements (bust 36.5, waist 30, hips 39.5) are entered and the "See My Sizes" button is clicked that Size 8 for the Banana Republic brand is displayed', async ({ page }) => {
+      await page.fill('#bust', '36.5');
+      await page.fill('#waist', '30');
+      await page.fill('#hips', '39.5');
+      await page.getByRole('button').click();
+
+      const bananaRepublicSize8 = page.getByText('Size 8').nth(2); 
+      await bananaRepublicSize8.scrollIntoViewIfNeeded();
+      await expect(bananaRepublicSize8).toBeVisible();
+    });
+
+    test('should show when in-range measurements (bust 36.5, waist 30, hips 39.5) are entered and the "See My Sizes" button is clicked that Size 8 for the J Crew brand is displayed', async ({ page }) => {
+        await page.fill('#bust', '36.5');
+        await page.fill('#waist', '30');
+        await page.fill('#hips', '39.5');
+        await page.getByRole('button').click();
+
+        const jCrewSize8 = page.getByText('Size 8').last(); 
+        await jCrewSize8.scrollIntoViewIfNeeded();
+        await expect(jCrewSize8).toBeVisible();
+    });
+
 });
